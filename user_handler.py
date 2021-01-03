4624d994016c1a3c630969cbd572d349fd41b3cd
Hector Castillo Martinez <hcastillomartinez@unm.edu>
2021-01-02 22:27:03 -0600

    authorized user access

--- user_handler.py
+++ user_handler.py
@@ -1,2 +1,12 @@
 import praw
+import config
 
+reddit = praw.Reddit(
+    username=config.USERNAME,
+    password=config.PASSWORD,
+    client_id=config.CLIENT_ID,
+    client_secret=config.CLIENT_SECRET,
+    user_agent=config.USER_AGENT,
+)
+
+print(reddit.read_only)
